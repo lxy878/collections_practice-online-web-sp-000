4070e985c46230982c13826a3091be79bff14a25
Xiaoyi Liu <lxy878@gmail.com>
2020-03-05 15:51:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,3 +5,19 @@ end
 def sort_array_desc(array)
   return array.sort.reverse
 end
+
+def sort_array_char_count(array)
+
+end
+
+def swap_elements(array)
+
+end
+
+def reverse_array(array)
+
+end
+
+def kesha_maker(array)
+
+end
