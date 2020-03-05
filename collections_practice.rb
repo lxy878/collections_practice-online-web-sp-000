e579c1eaaff423e879e714aa89406aa1588d6bf6
Xiaoyi Liu <lxy878@gmail.com>
2020-03-05 16:37:11 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
+def sort_array_asc(array)
+  return array.sort
+end
 
+def sort_array_desc(array)
+  return array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  return array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  return array.reverse
+end
+
+def kesha_maker(array)
+  array.collect {|word| "#{word[0..1]}$#{word[3..-1]}"}
+end
+
+def find_a(array)
+  array.select {|word| word.start_with?("a")}
+end
+
+def sum_array(array)
+  array.reduce {|sum, n| sum + n}
+end
+
+def add_s(array)
+  collection =[]
+  array.each_with_index do |word, index|
+    if index ==1
+      collection << word
+    else
+      collection << "#{word}s"
+    end
+  end
+  collection
+end
