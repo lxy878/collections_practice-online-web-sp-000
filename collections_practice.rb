1daf38e21d6e98c9fe0b3c885ff861a6ac0c3711
Xiaoyi Liu <lxy878@gmail.com>
2020-03-05 16:16:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,7 +22,7 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-  array.collect {|word| "#{word[0..1]}$#{word[4..-1]}"}
+  array.collect {|word| "#{word[0..1]}$#{word[3..-1]}"}
 end
 
 def find_a(array)
